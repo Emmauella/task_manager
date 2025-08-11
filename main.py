bb70fe30279c0b8af0fd0668905a3a8c66fb1616
Emmanuella <emmanuelladodoo11@gmail.com>
2025-08-11 11:00:38 +0000

    Getting started with task manager

---
+++ main.py
@@ -0,0 +1,31 @@
+# Program to calculate the average of two numbers
+
+# Take two numbers as input
+# Declare the average variable
+# Delare average = (num1 + num2)/ 2
+# Display ther average
+
+# num1 = float(input("Enter the first number: \n"))
+# num2 = float(input("Enter the second number: \n"))
+# average = float((num1 + num2) / 2)
+# print(f"The average of {num1} and {num2} is {average}")
+
+# # Accept an age input from the user and a full name
+# # If the age is less than 18 and the average is equal to 20
+# # print {name}, you are not allowed to vote
+# # If the age is greater than or equal to 18 and the average is greater than or 20,
+# # Print {name}, you are allowed to vote
+# # If the age is greater than or equal to 18 or the average is 10,
+# # Print {name}, You are allowed to vote
+
+# username = input("What is your name? \n")
+# age = int(input("Enter your age: \n"))
+
+# if age < 18 and average == 20:
+#     print(f"{username}, You are not allowed to vote.")
+
+# elif age >= 18 and average >= 20:
+#     print(f"{username}, You are allowed to vote.")
+
+# elif age >= 18 or average == 10:
+#     print(f"{username}, You are allowed to vote.")
